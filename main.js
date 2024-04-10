81ca0dfdeccaaf2929c0908a67628d6eb6fd01d7
Riya258 <riyachoudhary8288@gmail.com>
2024-04-10 16:27:05 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,38 @@
+const {app,BrowserWindow} = require('electron')
+
+const path=require('path')
+
+const {autoUpdater} = require('electron-updater')
+const log = require('electron-log')
+log.transports.file.resolvePath=() => path.join("C:/Users/lenovo/Desktop/Auto-Updater-Electron", 'logs/main.log');
+
+log.info("Hello, log");
+log.warn("some problem appears");
+let win;
+function createWindow(){
+    win= new BrowserWindow({width:300,height:400})
+    console.log(path.join(__dirname,'index.html'))
+    win.loadFile(path.join(__dirname,'index.html'))
+}
+
+app.on('ready', ()=>{
+    createWindow()
+    autoUpdater.checkForUpdatesAndNotify()
+})
+
+
+autoUpdater.on("update-available",()=>{
+    log.info("update-available")
+})
+
+autoUpdater.on("checking-for-update",()=>{
+    log.info("checking-for-update")
+})
+
+autoUpdater.on("download-progress",()=>{
+    log.info("download-progress")
+})
+
+autoUpdater.on("update-downloaded",()=>{
+    log.info("update-downloaded")
+})
